555b5b1bbe640f2706187d2f19944c535c892fea
val <valentin@agence-webup.com>
2023-11-12 18:02:14 +0100

    feat: 2021-01

---
+++ app/Solutions/Solution202101.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Solutions;
+
+class Solution202101
+{
+    public function silver(string $data): string
+    {
+        $lines = collect(explode("\n", $data))
+            ->filter(fn ($line) => strlen($line) > 0)
+            ->map(fn ($line) => intval($line));
+
+        $increaseCount = 0;
+        for ($i = 1; $i < $lines->count(); $i++) {
+            if ($lines[$i] > $lines[$i - 1]) {
+                $increaseCount++;
+            }
+        }
+
+        return $increaseCount;
+    }
+
+    public function gold(string $data): string
+    {
+        $lines = collect(explode("\n", $data))
+            ->filter(fn ($line) => strlen($line) > 0)
+            ->map(fn ($line) => intval($line));
+
+        $increaseCount = 0;
+        for ($i = 0; $i < $lines->count() + 3; $i++) {
+            $current = $lines->get($i) + $lines->get($i + 1) + $lines->get($i + 2);
+            $next = $lines->get($i + 1) + $lines->get($i + 2) + $lines->get($i + 3);
+            if ($current < $next) {
+                $increaseCount++;
+            }
+        }
+
+        return $increaseCount;
+    }
+}
