e5e71fe08d49b6ea9117c6192593a5560daaba84
val <valentin@agence-webup.com>
2023-11-12 23:33:57 +0100

    feat: 2021-07

---
+++ app/Solutions/Solution202107.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Solutions;
+
+class Solution202107
+{
+    public function silver(string $data): string
+    {
+        $crabs = collect(explode(',', explode("\n", $data)[0]))
+            ->map(fn ($position) => intval($position));
+
+        $align = 0;
+
+        $min_fuel = PHP_INT_MAX;
+
+        for ($i = 0; $i < $crabs->max(); $i++) {
+            $fuel = 0;
+            foreach ($crabs as $crab) {
+                $fuel += abs($crab - $i);
+            }
+            if ($fuel < $min_fuel) {
+                $min_fuel = $fuel;
+                $align = $i;
+            }
+        }
+
+        return sprintf('%d fuel needed to align with %d', $min_fuel, $align);
+    }
+
+    public function gold(string $data): string
+    {
+        $crabs = collect(explode(',', explode("\n", $data)[0]))
+            ->map(fn ($position) => intval($position));
+
+        $align = 0;
+
+        $min_fuel = PHP_INT_MAX;
+
+        for ($i = 0; $i < $crabs->max(); $i++) {
+            $fuel = 0;
+            foreach ($crabs as $crab) {
+                $n = abs($crab - $i);
+                $fuel += $n * ($n + 1) / 2; // https://en.wikipedia.org/wiki/Triangular_number#Formula
+            }
+            if ($fuel < $min_fuel) {
+                $min_fuel = $fuel;
+                $align = $i;
+            }
+        }
+
+        return sprintf('%d fuel needed to align with %d', $min_fuel, $align);
+    }
+}
