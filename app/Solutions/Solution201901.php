5f693b7adfd55ebee51bcc7fd9e07a946d77064c
val <valentin@agence-webup.com>
2023-11-12 17:30:45 +0100

    feat: 2019-01

---
+++ app/Solutions/Solution201901.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Solutions;
+
+function getFuelCost(int $mass, bool $recursive = false): int
+{
+    $theoretical = intval($mass / 3) - 2;
+
+    if ($theoretical <= 0) {
+        return 0;
+    }
+
+    if ($recursive) {
+        return $theoretical + getFuelCost($theoretical, true);
+    }
+
+    return $theoretical;
+}
+
+class Solution201901
+{
+    public function silver(string $data): string
+    {
+        $lines = explode("\n", $data);
+
+        $totalFuel = 0;
+        foreach ($lines as $line) {
+            if (strlen($line) == 0) {
+                continue;
+            }
+
+            $mass = intval($line);
+            $totalFuel += getFuelCost($mass);
+        }
+
+        return $totalFuel;
+    }
+
+    public function gold(string $data): string
+    {
+        $lines = explode("\n", $data);
+
+        $totalFuel = 0;
+        foreach ($lines as $line) {
+            if (strlen($line) == 0) {
+                continue;
+            }
+
+            $mass = intval($line);
+            $totalFuel += getFuelCost($mass, true);
+        }
+
+        return $totalFuel;
+    }
+}
