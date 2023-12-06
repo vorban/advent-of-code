ad3d9484ef8aff6cb1131c90012dd4b2e586ff9a
val <valentin@agence-webup.com>
2023-12-07 00:40:36 +0100

    perf(2023-06): quadratics, thanks mrs math teacher <3

--- app/Solutions/Year_2023/Solution_06.php
+++ app/Solutions/Year_2023/Solution_06.php
@@ -6,16 +6,34 @@ use Illuminate\Support\Str;
 
 function ways_to_beat($time, $record): int
 {
-    $min_to_beat = 0;
-    for ($i = 0; $i <= $time; $i++) {
-        $distance = ($time - $i) * $i;
-        if ($distance > $record) {
-            $min_to_beat = $i;
-            break;
-        }
-    }
+    // canonical quadratic equation
+    // (t - x) * x > r
+    // (t - x) * x - r > 0
+    // -xx + tx - r > 0      <---- this is ax^2 + bx + c > 0
+    //                             with a = -1; b = t; c = -1
 
-    return $time + 1 - $min_to_beat * 2;
+    // delta = bb - 4ac
+    // delta = tt - 4*-1*-r = tt - 4r
+    $delta = $time * $time - 4 * $record;
+    // I'm assuming $delta > 0, otherwise the puzzle is impossible to solve
+    // because $delta == 0 means 1 root where equation == 0, so cannot beat
+    // the score. $delta < 0 means we never even match the record.
+
+    // a is negative, so the curve is a peak as such: /\, not a bowl \/
+    // so the first time it crossed 0 is when
+
+    // root1 = (-b - sqrt(delta)) / 2a; b = t; a = -1
+    // root2 = (-b + sqrt(delta)) / 2a
+    $left_root = (0 - $time - sqrt($delta)) / -2;
+    $right_root = (0 - $time + sqrt($delta)) / -2;
+
+    // $the 1st root is where we match the record
+    $root = intval(min($left_root, $right_root));
+
+    // ways not to beat = $root on the left
+    //                  + $root on the right
+    //                  - 1 index shenanigans
+    return $time - $root * 2 - 1;
 }
 
 class Solution_06
