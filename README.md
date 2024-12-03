697f79ce391daa555b7b1cfaa1378d15c1c12c20
Valentin Orban <nyvelant@proton.me>
2024-12-03 23:23:51 +0100

    feat: impl 2024-01

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
+<div>
+    <img src="https://img.shields.io/badge/total_stars%20⭐-02-fcd34d?style=for-the-badge">
+    <br />
+    <img src="https://img.shields.io/badge/2024%20⭐-02-a8a29e">
+</div>
+
 # advent-of-code
 
-Advent of code solutions in Go
+[Advent of code](https://adventofcode.com/) solutions in Go
+
+## Usage
+
+```sh
+go build ./cmd/solver.go
+solver {year} {day} {silver|gold} [sample]
+```
+
+Both `year` and `day` should be left-padded with zeros (ex: `2024 03`).
+Specify `sample` to run with the small example given in the subject.
--- internal/2024-01.go
+++ internal/2024-01.go
@@ -1,12 +1,76 @@
 package solutions
 
-import solutionRegister "vorban/advent-of-code/pkg"
+import (
+	"fmt"
+	"sort"
+	"strconv"
+	"strings"
+	solutionRegister "vorban/advent-of-code/pkg"
+)
+
+func parse(input string) ([]int, []int) {
+	// Read input and parse it into a 2D int array
+	left := []int{}
+	right := []int{}
+
+	lines := strings.Split(input, "\n")
+
+	for _, line := range lines {
+		if line == "" {
+			continue
+		}
+
+		parts := strings.Split(line, "   ")
+		l, _ := strconv.Atoi(parts[0])
+		r, _ := strconv.Atoi(strings.TrimSpace(parts[1]))
+
+		left = append(left, l)
+		right = append(right, r)
+	}
+
+	return left, right
+}
+
+func count(list []int, value int) int {
+	count := 0
+	for _, v := range list {
+		if v == value {
+			count++
+		}
+	}
+
+	return count
+}
 
 var Day202401 = solutionRegister.Solution{
 	Silver: func(input string) string {
-		return input
+		left, right := parse(input)
+
+		sort.Ints(left)
+		sort.Ints(right)
+
+		result := 0
+		for i, l := range left {
+			diff := (l - right[i])
+			if diff < 0 {
+				result += -diff
+			} else {
+				result += diff
+			}
+		}
+		return fmt.Sprintf("%d", result)
 	},
 	Gold: func(input string) string {
-		return ""
+		left, right := parse(input)
+
+		sort.Ints(left)
+		sort.Ints(right)
+
+		result := 0
+		for _, l := range left {
+			similarity := count(right, l)
+			result += similarity * l
+		}
+		return fmt.Sprintf("%d", result)
 	},
 }
