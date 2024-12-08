b6d0149a3975b8b45069753f2e6818ca0804d5d6
Valentin Orban <nyvelant@proton.me>
2024-12-08 12:38:24 +0100

    wip: prepare authenticated fetching and .env loading

--- tools/updateBadges/updateBadges.go
+++ tools/updateBadges/updateBadges.go
@@ -2,10 +2,17 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"log"
+	"net/http"
 	"os"
+	"regexp"
 	"strconv"
 	"strings"
 	"time"
+
+	"github.com/joho/godotenv"
+	"github.com/pterm/pterm"
 )
 
 const MARKER = "<!-- ----- marker: badges ----- -->"
@@ -20,27 +27,103 @@ const URL = "https://img.shields.io/badge"
 type Badge struct {
 	Year  int
 	Stars int
-	Color string
 }
 
 func (b Badge) ToBadgeString() string {
 	return fmt.Sprintf(
 		"<img src=\"%s/%d%%20⭐-%02d-%s\">\n",
-		URL, b.Year, b.Stars, b.Color)
+		URL, b.Year, b.Stars, b.Color())
 }
 
-func getColor(stars int) string {
+func (b Badge) Color() string {
 	switch {
-	case stars >= 50:
+	case b.Stars >= 50:
 		return COLOR_GOLDEN
-	case stars > 0:
+	case b.Stars > 0:
 		return COLOR_SILVER
 	default:
 		return COLOR_GREY
 	}
 }
 
+func getHtml() string {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	// ask for user data
+	var username, repository, email, session string
+
+	if username = os.Getenv("GITHUB_USERNAME"); username == "" {
+		username, _ = pterm.DefaultInteractiveTextInput.Show("Enter your github.com username")
+		envFile, err := os.ReadFile(".env")
+		if err != nil {
+			panic(err)
+		}
+		env, _ := godotenv.UnmarshalBytes(envFile)
+		env["GITHUB_USERNAME"] = username
+		_ = godotenv.Write(env, "./.env")
+	}
+	if repository = os.Getenv("GITHUB_REPOSITORY"); repository == "" {
+		repository, _ = pterm.DefaultInteractiveTextInput.Show("Enter your github.com repository")
+	}
+	if email = os.Getenv("GITHUB_EMAIL"); email == "" {
+		email, _ = pterm.DefaultInteractiveTextInput.Show("Enter your github.com email")
+	}
+	if session = os.Getenv("ADVENT_SESSION"); session == "" {
+		session, _ = pterm.DefaultInteractiveTextInput.WithMask("*").Show("Enter your adventofcode.com session ID")
+	}
+
+	if session == "" {
+		panic("Session ID is required")
+	}
+	if username == "" {
+		panic("Username is required")
+	}
+	if repository == "" {
+		panic("Repository is required")
+	}
+	if email == "" {
+		panic("Email is required")
+	}
+
+	req, _ := http.NewRequest(http.MethodGet, "https://adventofcode.com/events", nil)
+	req.Header.Set("User-Agent", fmt.Sprintf("github.com/%s/%s by %s", username, repository, email))
+	req.Header.Set("Cookie", fmt.Sprintf("session=%s", session))
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		panic(err)
+	}
+
+	body, err := io.ReadAll(res.Body)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return string(body)
+}
+
+func getStars(html string) map[int]int {
+	years := map[int]int{}
+
+	r := regexp.MustCompile(`\[(\d{4})\]\D*([0-9]+)\*`)
+	matches := r.FindAllStringSubmatch(html, -1)
+
+	for _, match := range matches {
+		fmt.Printf("Year: %s, Stars: %s\n", match[1], match[2])
+	}
+
+	return years
+}
+
 func main() {
+	html := getHtml()
+	stars := getStars(html)
+	fmt.Printf("%v\n", stars)
+
 	// detect the last year
 	var lastYear int
 	if time.Now().Month() == time.December {
@@ -49,7 +132,6 @@ func main() {
 		lastYear = time.Now().Year() - 1
 	}
 
-	// prompt user for the number of stars for each year
 	years := []Badge{}
 	for year := FIRST_YEAR; year <= lastYear; year++ {
 		var input string
@@ -67,7 +149,6 @@ func main() {
 		years = append(years, Badge{
 			Year:  year,
 			Stars: parsed,
-			Color: getColor(parsed),
 		})
 	}
 
